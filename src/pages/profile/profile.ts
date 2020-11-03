0946794735afafd644a0b23a9cb983d90701c62d
Julio Alves <jccallves@gmail.com>
2020-11-03 12:36:56 -0300

    Usando Cordova para tirar foto com a câmera

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,6 +4,7 @@ import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { API_CONFIG } from '../../configs/api.config';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the ProfilePage page.
@@ -20,9 +21,11 @@ import { ClienteService } from '../../services/domain/cliente.service';
 export class ProfilePage {
 
   cliente: ClienteDTO;
+  picture: string;
+  cameraOn: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public storage: StorageService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService, public camera: Camera) {
   }
 
   ionViewDidLoad() {
@@ -52,6 +55,24 @@ export class ProfilePage {
         this.cliente.imageUrl = `${API_CONFIG.bucketClientes}/cp${this.cliente.id}.jpg`;
       },
         error => { });
+  }
+
+  getCameraPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+    });
   }
 
 }
